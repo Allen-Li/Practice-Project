7851a892e6aa04c435eab7e69463ced924dbf1a3
Allen Li <allen@beansmile.com>
2014-06-10 11:18:59 +0800

    my practice project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PracticeProject::Application.config.secret_token = 'f794c7f31d0f79639509da61cf57707f69708c52be68f53db9258bc5633cb224684e5fd4ac7c5bc3e0e5d147d6862c35c32547ddfbef55905514de6057189719'
